4724db73633a50c05a672837b2c5e69db993b7c3
crnunez <53486087+crnunez@users.noreply.github.com>
2021-04-05 19:04:17 -0300

    Update de Scroll

--- app/src/main/java/com/bitgymup/gymup/MainActivity.java
+++ app/src/main/java/com/bitgymup/gymup/MainActivity.java
@@ -1,14 +1,42 @@
 package com.bitgymup.gymup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.bitgymup.gymup.users.UserHome;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.menuinf);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.menu_login){
+                    Intent ingreso = new Intent(getApplicationContext(), LogIn.class);
+                    startActivity(ingreso);
+                    //finish();
+                }
+                if (item.getItemId() == R.id.menu_registro){
+                    Intent registro = new Intent(getApplicationContext(), Register.class);
+                    startActivity(registro);
+                    //finish();
+                }
+
+                return true;
+            }
+        });
+
+
     }
 }
