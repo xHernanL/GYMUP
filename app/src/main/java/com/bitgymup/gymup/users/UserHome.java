febedcd01a8a6e41b6066eb507adf745f3d47b00
crnunez <53486087+crnunez@users.noreply.github.com>
2021-04-08 17:22:44 -0300

    Idioma inglés, agregado de strings en Toast. Actualización de UserHome.java y su .xml

--- app/src/main/java/com/bitgymup/gymup/users/UserHome.java
+++ app/src/main/java/com/bitgymup/gymup/users/UserHome.java
@@ -6,23 +6,116 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.bitgymup.gymup.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
 
 public class UserHome extends AppCompatActivity {
     //Inicializar las variables
     DrawerLayout drawerLayout;
+    private String username, vUsername;
+    private TextView tvUserEmail, tvUserPhone, tvUserCompleteName, tvUserIMC, tvUserHeight, tvUserWeight;
+    private RequestQueue request;
+    ProgressDialog progreso;
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
         //Asignación de la variable
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        //tvUserEmail        = findViewById(R.id.tvUserEmail);
+        //tvUserPhone        = findViewById(R.id.tvUserPhone);
+        tvUserCompleteName = findViewById(R.id.tvUserCompleteName);
+        tvUserIMC          = findViewById(R.id.tvUserIMC);
+        tvUserHeight       = findViewById(R.id.tvUserHeight);
+        tvUserWeight       = findViewById(R.id.tvUserWeight);
+        //btnEditProfile     = findViewById(R.id.btnEditProfile);
+        //btnEditPassword    = findViewById(R.id.btnEditPassword);
+
+        SharedPreferences userId1 = getSharedPreferences("user_login", Context.MODE_PRIVATE);
+        username = userId1.getString("username", "");
+        //   Toast.makeText(getApplicationContext(), "username: " + username  , Toast.LENGTH_LONG).show();
+        request = Volley.newRequestQueue(this);
+        loadUserData(username);
+
+
+    }//Fin OnCreate
+
+
+
+
+
+
+    private void loadUserData(String username) {
+        String url = "http://gymup.zonahosting.net/gymphp/getClientsDataWS.php?username=" + username.trim();
+        progreso = new ProgressDialog(UserHome.this);
+        progreso.setMessage("Cargando...");
+        progreso.show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject  = response.getJSONObject(0);
+
+                    String id      = jsonObject.optString("id");
+                    String name    = jsonObject.optString("name");
+                    String surname = jsonObject.optString("surname");
+                    String email   = jsonObject.optString("email");
+                    String phone   = jsonObject.optString("phone");
+                    String height  = jsonObject.optString("height");
+                    String weight  = jsonObject.optString("weight");
+
+                    String completeName = name.trim() + " " + surname.trim();
+
+                    tvUserCompleteName.setText(completeName);
+                    tvUserHeight.setText(height);
+                    tvUserWeight.setText(weight);
+                    //tvUserEmail.setText(email);
+                    //tvUserPhone.setText(phone);
+
+                    //    Toast.makeText(getApplicationContext(), "email: " + email  , Toast.LENGTH_LONG).show();
+                    progreso.hide();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } ;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+
     }
+
+
 
     public void ClickMenu(View view){
         //Abrir drawer
@@ -86,9 +179,9 @@ public class UserHome extends AppCompatActivity {
         //Set Titulo
         builder.setTitle("Salir");
         //Set mensaje
-        builder.setMessage("¿Estás seguro que deseas salir?");
+        builder.setMessage(R.string.estasseguro);
 
-        builder.setPositiveButton("SI", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(R.string.Si, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 //finaliza la activity
@@ -98,7 +191,7 @@ public class UserHome extends AppCompatActivity {
             }
         });
         //Respuesta Negativa
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 //Salida del diálogo
